5a49e826c6abf47ab8735b404eb55e51decaef18
Dane Collins <dane@pobox.com>
2018-04-26 18:08:01 -0700

    add checks on database

--- check.py
+++ check.py
@@ -6,6 +6,37 @@ from collections import defaultdict, Counter
 db = db_bind()
 
 
+def ord(x):
+    return x[0] + x[1]/100
+
+def next_month(m):
+    if m[1] == 12:
+        return (m[0]+1, 1)
+    else:
+        return (m[0], m[1] + 1)
+
+def check_months(months, header):
+    error = False
+    l = sorted(months.copy(), key=ord, reverse=True)
+    start = l.pop()
+    expected = next_month(start)
+    month_to_check = l.pop()
+    while month_to_check:
+        if month_to_check != expected:
+            if not error:
+                print(header)
+            error = True
+            print('    expected {} but got {}'.format(expected, month_to_check))
+        expected = next_month(month_to_check)
+        if l:
+            month_to_check = l.pop()
+        else:
+            month_to_check = None
+    if error:
+        print("   ", months)
+    return error
+
+
 # for each car, check number of sources
 def check_num_sources():
     with db_session:
@@ -34,6 +65,14 @@ def check_num_sources():
             print()
 
 
-if __name__ == '__main__':
+def check_date_sequences():
+    with db_session:
+        cars = select((s.make, s.model, s.source) for s in SalesFigure)[:]  # distinct cars and sources
+        for (ma, mo, so) in cars:
+            dates = select((s.year, s.month) for s in SalesFigure if (s.make==ma) and (s.model==mo) and (s.source==so))[:]
+            header = 'checking {} {} - {}'.format(ma, mo, so)
+            check_months(dates, header)
 
+if __name__ == '__main__':
     check_num_sources()
+    check_date_sequences()
